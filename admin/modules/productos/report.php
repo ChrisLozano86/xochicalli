4feaf9876dd3f09a028f370567bab245469bc18f
ChrisLozano86 <ChrisLozano86>
2022-04-12 17:34:28 -0500

    Xochicalli_v1.0.10

--- admin/modules/productos/report.php
+++ admin/modules/productos/report.php
@@ -1,7 +1,7 @@
 <?php
 
 ob_start();
-require_once '../../class/Producto.php.php';
+require_once '../../class/Producto.php';
 $productos = Producto::recuperarTodos();
 ?>
 
@@ -107,7 +107,7 @@ th, td {
 
 
 <?php
-/* // Cargamos la librería dompdf que hemos instalado en la carpeta dompdf
+// Cargamos la librería dompdf que hemos instalado en la carpeta dompdf
 require_once 'dompdf/autoload.inc.php';
 use Dompdf\Dompdf;
 
@@ -132,4 +132,3 @@ $filename = 'reporte_'.date('d-m-Y');
 // Enviamos el fichero PDF al navegador.
 $pdf->stream($filename);
 
- */
