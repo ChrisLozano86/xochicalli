05ff21e98605e1c8187d062c26f73c7e2d74bfcb
ChrisLozano86 <ChrisLozano86>
2022-04-12 17:57:19 -0500

    Xochicalli_v1.0.10

--- admin/modules/productos/report.php
+++ admin/modules/productos/report.php
@@ -66,7 +66,7 @@ th, td {
           <tr>
                 
                 <td><?php echo $item['nombre'];  ?></td>
-                <td class="text-justify"><?php echo $item['descripcion'];  ?></td>
+                <td><p class="text-justify"><?php echo $item['descripcion'];  ?> </p></td>
                 <td>$<?php echo $item['precio'];  ?></td>
                 <td><?php echo $item['presentacion'];  ?></td>
 
